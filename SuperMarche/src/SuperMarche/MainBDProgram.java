491209e34598311d3e5fc5bf2210d8cd205a60b5
JrYassine <yassine.jrad06@gmail.com>
2016-12-04 15:59:19 +0100

    superMarche

--- SuperMarche/src/SuperMarche/MainBDProgram.java
+++
@@ -1,8 +0,0 @@
-package isn;
-public class MainBDProgram {
-   public static boolean debug = false; // active/desactive les traces de debug
-
-   public static void main(String[] args) {
-      FenetreMagasin fen = new FenetreMagasin(); //crée la fenetre principale
-     }
-}
